e33bb606b20cce2c4d5a3a369768c328853a9488
luciasalinash <luciasalinashernandez@.gmail.com>
2021-12-02 20:15:02 -0500

    JavaSript Part 1

---
+++ main.js
@@ -0,0 +1,36 @@
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    }
+    ,
+    {
+        id: 2,
+        text: 'Metting w boss',
+        isCompleted: true
+    }
+    ,
+    {
+        id: 3,
+        text: 'Dentist appt',
+        isCompleted: false
+    }
+];
+
+// console.log(todos[1].text);
+
+// const todoJSON = JSON.stringify(todos);
+// console.log(todoJSON);
+
+// For
+for (let i=0; i<=10; i++){
+  console.log(i);
+}
+
+// While
+let i=0;
+while( i < 10){
+    console.log(`While loop number: ${i}`);
+    i++;
+}
